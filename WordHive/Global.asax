7e58f50bb2c138cb4557b6eced93c881356d24be
Alex Addison <alexanderTaddison@gmail.com>
2015-12-11 10:58:52 -0500

    Initial commit
    Not much to see here.

---
+++ WordHive/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WordHive.MvcApplication" Language="C#" %>
---
+++ WordHive/Models/Puzzle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WordHive.Models
+{
+    public class Puzzle
+    {
+        public int PuzzleID { get; set; }
+        public int Difficulty { get; set; }
+        public int MinWordLength { get; set; }
+        public int MinVowels { get; set; }
+        public int MaxVowels { get; set; }
+        public char[] Required { get; set; }
+        public char[] Optional { get; set; }
+        public char[] Inverse { get; set; }
+        public char[] Forbidden { get; set; }
+        public string[] Answers { get; set; }
+    }
+}
